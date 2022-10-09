6879b75c273d7a612e2a7790a41968d2b09549b8
marimonon <mappi-@hotmail.co.jp>
2022-10-10 08:54:43 +0900

    CSSのコピー

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,103 @@ function App() {
   return (
     <section
       css={css`
-        background: pink;
+        width: 400px;
+        margin: 8px auto;
+        background-color: #fff;
+        border-radius: 4px;
+        padding: 16px;
+        position: relative;
       `}
     >
-      <p id="question"></p>
-      <ul id="choices"></ul>
-      <p id="comment"></p>
-      <div id="btn" css={{ backgroundColor: "blue" }}>
+      <p
+        id="question"
+        css={css`
+          margin-bottom: 16px;
+          font-weight: bold;
+        `}
+      ></p>
+      <ul
+        id="choices"
+        css={css`
+          list-style: none;
+          padding: 0;
+          margin-bottom: 16px;
+        `}
+      ></ul>
+      <p
+        id="comment"
+        css={css`
+          display: block;
+          padding: 10px;
+          background-color: #fff;
+        `}
+      ></p>
+      <div
+        id="btn"
+        css={css`
+          background: #3498db;
+          padding: 8px;
+          border-radius: 4px;
+          cursor: pointer;
+          text-align: center;
+          color: #fff;
+          box-shadow: 0 4px 0 #2880b9;
+        `}
+      >
         Next
       </div>
-      <div id="startBtn" className="disabled">
+      <div
+        id="startBtn"
+        className="disabled"
+        css={css`
+          background: #3498db;
+          padding: 8px;
+          border-radius: 4px;
+          cursor: pointer;
+          text-align: center;
+          color: #fff;
+          box-shadow: 0 4px 0 #2880b9;
+        `}
+      >
         Start
       </div>
-      <section id="result" className="hidden">
-        <p></p>
-        <a href="">Replay?</a>
+      <section
+        id="result"
+        className="hidden"
+        css={css`
+          position: absolute;
+          width: 300px;
+          background: #fff;
+          padding: 30px;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+          top: 50px;
+          margin: 0 auto;
+          border-radius: 4px;
+          text-align: center;
+          transition: 0.4s;
+        `}
+      >
+        <p
+          css={css`
+            font-size: 24px;
+          `}
+        ></p>
+        <a
+          href=""
+          css={css`
+            background: #3498db;
+            padding: 8px;
+            border-radius: 4px;
+            cursor: pointer;
+            text-align: center;
+            color: #fff;
+            box-shadow: 0 4px 0 #2880b9;
+            display: block;
+            text-decoration: none;
+          `}
+        >
+          Replay?
+        </a>
       </section>
     </section>
   );
